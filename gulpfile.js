27f6f064703ccd958c16280820dffa7d289462f5
ichderfisch <Dennis_Fischer@web.de>
2016-07-14 23:38:09 +0200

    add clean-css to build

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var cleanCSS = require('gulp-clean-css');
 
-gulp.task('sass', function () {
+gulp.task('sass:dev', function () {
   return gulp.src('./public/scss/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
@@ -14,4 +15,11 @@ gulp.task('sass', function () {
 
 gulp.task('sass:watch', function () {
   gulp.watch('./public/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('build', function () {
+  return gulp.src('./public/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./public/css'));
 });
